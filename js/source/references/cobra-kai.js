e1aa9ffe9bacbabb00c76f738cd68b3474822710
punkapocalyptic <67438405+punkapocalyptic@users.noreply.github.com>
2020-09-18 19:53:04 +0200

    Referencia a Cobra Kai.

---
+++ js/source/references/cobra-kai.js
@@ -0,0 +1,12 @@
+source.push({
+    "id": "cobra-kai",
+    "name": {
+        "en": "Cobra Kai",
+        "es": "Cobra Kai"
+    },
+    "type": "reference",
+    "url": {
+        "en": "https://en.wikipedia.org/wiki/Cobra_Kai",
+        "es": "https://es.wikipedia.org/wiki/Cobra_Kai"
+    }
+});
