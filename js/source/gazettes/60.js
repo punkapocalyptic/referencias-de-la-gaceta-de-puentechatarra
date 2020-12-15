ec2e519a0d693c9209cbf793178bef898b2a915e
punkapocalyptic <67438405+punkapocalyptic@users.noreply.github.com>
2020-12-15 20:43:28 +0100

    Gaceta número 60
    introducción de la gaceta 60 y revisión de la 59.

---
+++ js/source/gazettes/60.js
@@ -0,0 +1,70 @@
+source.push({
+    "id": 60,
+    "sections": {
+        "breaking news": [
+            {
+                "en": '<p>So, let’s see. We have to rectify last month’s no-tice about our public readers. <punka-character data-id="la-yoly">The Yoly</punka-character> says that, after not being able to sell a fuckin’ single issue of her <punka-object data-id="libro-juego-de-rollos-imaginarios">game-book of imaginary adventures</punka-object>, <punka-proverb data-id="la-gente-es-pobre-como-cucarratas">people are poor as <punka-nature data-id="cucarrata">ratcroaches</punka-nature></punka-proverb>. Well, she also sweared a lot of curses, bad words, insults and things like that. But we prefer to keep the few readers we still have left, so we are not going to print here all the nasty things she called them. But we don’t want to anger <punka-character data-id="la-yoly">Yoly</punka-character> neither, so we won’t mention that maybe the book was not as good as she thinks it was.</p>',
+                "es": '<p>Pues na. Tenemos que rectificar la noticia del mes pasado  sobre  nuestro público. Dice <punka-character>la Yoly</punka-character>, después de no conseguir vender una mierda de su <punka-object>libro-juego de rollos imaginarios</punka-object>, que <punka-proverb>la gente es pobre como <punka-nature data-id="cucarrata">cucarratas</punka-nature></punka-proverb>. Bueno, también suelta muchos insultos, cagamentos, tacos y cosas de esas. Pero como preferimos mantener a los lectores que nos quedan, no les vamos a llamar todo lo que nos ha puesto. Y como tampoco queremos enfadar a <punka-character>la Yoly</punka-character>, no mencionaremos nada de que igual no se vendió porque no estaba tan bien hecho como ella cree.</p>'
+            },
+            {
+                "en": '<p>We are informed that, after a few sightings, if it doesn’t change direction, <punka-place data-id="errantia">Wanderbury</punka-place> will be crossing some miles south of <punka-place>Samanthia</punka-place> in  a  couple  of weeks. Remember <punka-place data-id="errantia">Wanderbury</punka-place> is a settlement which growed on top of the back of a <punka-nature data-id="bicho-enormerrimo-pero-grande-grande">huge-ass creature (we mean, big as fuck)</punka-nature> that goes wandering ‘bout <punka-place data-id="el-paramo">the Wasteland</punka-place> minding its own business. We will try to send a vonlunteer  journalist out there to interview the citizens, and we will tell ya in our next issue of <punka-media data-id="la-gaceta-de-puentechatarra">the Gazette</punka-media>.</p>',
+                "es": '<p>Nos informan de que después de algunos avistamientos, si no le da por cambiar de dirección, <punka-place>Errantia</punka-place> pasará por la zona al sur de <punka-place>Samanthia</punka-place> en un par de semanas. Recordad que <punka-place>Errantia</punka-place> es un asentamiento que se han montado en lo alto de un <punka-nature>bicho enormérrimo (pero grande, grande)</punka-nature> que anda vagando por <punka-place>el Páramo</punka-place> ahí a su ritmo y bola. Intentaremos que un reportero voluntario vaya hacia allí y pueda hablar un poco con sus gentes, y os lo contamos en el próximo número.</p>'
+            },
+            {
+                "en": '<p>From the <punka-place data-id="dunas-del-este">dunes to the East</punka-place> we get reports about a <punka-character data-id="tio-loco"><punka-reference data-id="dune" data-name="Dune">crazy fella</punka-reference></punka-character> that wants to ride the <punka-nature data-id="gusanasco-de-las-arenas" data-name="sand mawworms">Sand mawworm</punka-nature>. I know the kind of people reading these pages, and I want to mke clear that by “ride” I mean travelling on top of ‘em, not bangin‘em. Maybe riding is just for <punka-nature data-id="caballo" data-name="Horse">horses</punka-nature>. Talking about <punka-nature data-id="gusanasco-de-las-arenas">mawworms</punka-nature>, it might be worming, aight? Man, that’s cool. Worming it is, then.</p><p>So, this guy goes around boasting shit about him being  the  chosen  one,  messiah  or  somethin’  like  that.  But  we  also  heard  that  she  really  likes  to  consume,  specially  a  really  weird  local drug that goes by the name “<punka-object>espize</punka-object>”  and  that  makes  his  eyes turn gleaming blue and scary as fuck to look to.</p><p>Thing is that he is putting up quite a show around the  area,  not  because  he  has  managed  to  get  many followers, but because people wants to enjoy the entertainment of watching him trying to worm during a espize trip.</p>',
+                "es": '<p>Desde las <punka-place>dunas del este</punka-place> nos comentan que hay un <punka-character><punka-reference data-id="dune" data-name="Dune">tío loco</punka-reference></punka-character> que quiere andar montando a los <punka-nature data-id="gusanasco-de-las-arenas" data-name="Gusañasco de las arenas">gusañascos de las arenas</punka-nature>. Como os conozco, por montar no quiero decir follárselos, sino cabalgarlos. O igual cabalgar es sólo para <punka-nature data-id="caballo" data-name="Caballo">caballos</punka-nature>. ¿Sería gusalgar si es con <punka-nature data-id="gusanasco-de-las-arenas">gusañascos</punka-nature>? Me ha molado, se queda como gusalgar.</p><p>El caso es que anda diciendo chorradas de que es un elegido, mesías o similar. Pero también nos comentan que por lo visto le gusta un poquillo el consumir,  y  que  por  esa  zona  se  ha  metido  una  droga  mu  rara  que  se llama <punka-object>espieza</punka-object> y que hace que los ojos se le pongan azules brillantes que da cosica aguantarle la mirada.</p><p>El caso es que se está creando gran expectación por  la  zona,  no  porque  le  sigan  ni  nada  de  eso,  sino  porque  la  gente  quiere  disfrutar  del  espectáculo  de  ver  cómo  en  medio  de  un  colocón  de  espieza se pone a intentar gusalgar.</p>'
+            }
+        ],
+        "classyfrieds": [
+            {
+                "message": {
+                    "en": '<p>I grow mushrooms behind my ears and in my belly button. Tasty and yummy.</p>',
+                    "es": '<p>Cultivo setas detrás de mis orejas y en mi ombligo. Mú ricas.</p>'
+                },
+                "reference": {
+                    "en": 'REF: <puka-character data-id="champi">Mushy</puka-character>',
+                    "es": 'REF: <punka-character>Champi</punka-character>'
+                }
+            },
+            {
+                "message": {
+                    "en": '<p>Saturday at six, in <punka-place data-id="terraza-de-la-pacheca"><punka-place data-id="la-pacheca">the Pacheca</punka-place> terrace</punka-place>, <punka-event data-id="torneo-a-hostia-limpia">face slappin’ tournament</punka-event>. Buy your tickets ahead of time, because blood doesn’t reach the last seats!</p>',
+                    "es": '<p>El sábado a las seis, en la <punka-place>terraza de <punka-place>la Pacheca</punka-place></punka-place>, <punka-event>torneo a hostia limpia</punka-event>. ¡Comprar las entradas con tiempo, que la sangre no llega a las últimas filas!</p>'
+                },
+                "reference": {
+                    "en": 'REF: <punka-character>Flex</punka-character>',
+                    "es": 'REF: <punka-character>Flex</punka-character>'
+                }
+            },
+            {
+                "message": {
+                    "en": '<p>Hunchbacks flattened by shoveling.</p>',
+                    "es": '<p>Se achatan chepas a palazos.</p>'
+                },
+                "reference": {
+                    "en": 'REF: <punka-character data-id="el-hostiopata">The Shoveler</punka-character>',
+                    "es": 'REF: <punka-character>El Hostiópata</punka-character>'
+                }
+            },
+            {
+                "message": {
+                    "en": '<p>The <punka-band>non-profit organization</punka-band> “<punka-band data-id="medicos-del-paramo"><punka-place data-id="el-paramo">Wasteland</punka-place> Medics</punka-band>” is opening a new <punka-event data-id="campana-de-donacion-de-sangre-y-organos">campaign of blood and organ donors</punka-event>. Please spread the word.</p>',
+                    "es": '<p>La <punka-band>Organización sinónimo de lucro</punka-band> "<punka-band>Médicos del <punka-place data-id="el-paramo">Páramo</punka-place></punka-band>" abre su nueva <punka-event>campaña de donación de sangre y órganos</punka-event>. Se agradece difusión.</p>'
+                },
+                "reference": {
+                    "en": 'REF: <punka-character>Francis Butcher Shop</punka-character>',
+                    "es": 'REF: <punka-character>Charcutería Francis</punka-character>'
+                }
+            }
+        ],
+        "monthly news": {
+            "en": '<p>Hear this shit out! A group of survivors from the <punka-faction data-id="idos">Amok</punka-faction> attack against the <punka-place data-id="picalculo">Asscratch</punka-place> settlement say that, the mean kickass lady leading the incursion, who goes by the name <punka-character data-id="madre">Mother</punka-character>, wants to talk with some of the local leaders of the <punka-place data-id="puentechatarra">Scrapbridge</punka-place> factions. Yes, we know what you are thinking, this is somethin’ like <punka-event data-id="prometeo-segunda-parte"><punka-character data-id="prometeo">Prometheus</punka-character> Part II</punka-event>. But this time our glorious Council of wise men and women has said, word by word, <q>She better go to talk to Granny, or specifically, her own fuckin’ Mother’s Mother, ‘cause we ain’t goin’ near her not for all the bullets in the World</q>. One thing is that the leader of the <punka-faction data-id="mutardos">Mutards</punka-faction>, which we have a few walking around our city and, umpopular as it is to say it aloud, not all of ‘em are bad fellas, wants to come to talk some issues with us, and that’s ok. But a completely different matter is that this lady-leader of a bunch of psycho-cannibals-sadistic-killing-assholes who only know to ra-vage, maim, rape and burn our settlements to the ground, has the balls to ask anything or wants to do shit with us. <punka-proverb data-id="nasti-de-plasti">No way, Manuel!</punka-proverb></p><p><punka-band data-id="el-consejo">The Council</punka-band> has sent some messengers to speak to the leaders of other important settlements and see what they plan to do. Out of curiosity, if anything, not because anyone sane enough would ever think about sittin’ at a table with the <punka-faction data-id="idos">Amok</punka-faction>.</p><p>Even so, this whole issue has taken almost everyone  by  surprise,  because  so  far  we  all  thought  the  Amok  were  just  savage  beasts  razing  anything  it  their wake, without a rational plan, much less the ability to speak.</p>',
+            "es": '<p>¡Flipadón del 15! Un grupo de supervivientes del ataque de los <punka-faction>Idos</punka-faction> al asentamiento de <punka-place>Picalculo</punka-place>, dicen que la tipa mega chunga que lideraba la incursión, y que se hace llamar <punka-character>Madre</punka-character>, quiere hablar con algunos de los líderes de la zona. Sí, ya sabemos lo que pensáis, que esto parece un <punka-event><punka-character>Prometeo</punka-character> segunda parte</punka-event>. Pero en esta ocasión <punka-band>el Consejo</punka-band> de  nuestro  glorioso  asentamiento  ha  dicho textualmente <q>Ya se puede reunir con ella Abuela,  o  lo  que  viene  a  ser  lo  mismo,  su  puta  madre,  que  a  esa  tipa  no  nos  acercamos  ni  de  coña</q>. Y es que una cosa es que el líder de los <punka-faction>Mutardos</punka-faction>, que tenemos algunos por aquí y aunque no sea popular decirlo, no todos son mala gente, quisiera venir, pues tenía un pase. Pero que tenga los ovarios de querer aparecer la tipa que parece estar  al  mando  de  una  panda  de  psicópatas-caníbales-sádicos-asesinos-malrolleros que lo único que  hace  es  arrasar  asentamientos, pues como que <punka-proverb>nasti de plasti</punka-proverb>.</p><p>Desde <punka-band>el Consejo</punka-band> han mandado a gente a hablar con los líderes de otros asentamientos importantes a ver qué van a hacer ellos. Pero por curiosidad más que nada, no porque vayan a apuntarse a una mesa redonda con ellos y los <punka-faction>Idos</punka-faction>.</p><p>Aún así, todo esto ha dejado bastante descolocado a todo el mundo, ya que hasta ahora se pensaba que los Idos iban por ahí sin ningún plan más allá de pasar por la piedra a todo el que encontraran en su camino.</p>'
+        },
+        "tip": {
+            "en": 'I think we are missin’ out somethin’ here. Have ya had a good look at me? Do ya really want ME to give advises to YA?',
+            "es": 'A ver, porque parece que no os enteráis. ¿Vosotros me habéis visto? ¿De verdad queréis que sea YO el que os dé consejos?'
+        }      
+    },
+    "type": "gazette"
+});
