d592e1b0c7512b7cc8f94f049a1673e7d59fa61e
punkapocalyptic <67438405+punkapocalyptic@users.noreply.github.com>
2022-04-22 01:14:00 +0200

    Añadida Gaceta 74 y revisiones de 73, 62, 59 y 57.

---
+++ js/source/gazettes/74.js
@@ -0,0 +1,66 @@
+source.push({
+    "id": 74,
+    "sections": {
+        "breaking news": [
+            {
+                "en": `<p>Dudes, what bad trip I had last month. There's a place in <punka-place data-id="sobatetas">Titgrab</punka-place> called <punka-place>Cinco Lobitas</punka-place>, which has just opened, and as a good informer who wants to give you the best news, I went to investigate there. There weren't five lolitas down there, but I can tell you that you could hear fucking great howling. Good vibes, better people and what to say about the booze... Fuck, that booze... A homemade moonshine that went in smooth as a girl's tights and as soon as you got up, you were on the floor again. This place is highly recommended. You absolutely have to go. Of course, if you have something to do (like editing a <punka-media data-id="la-gaceta-de-puentechatarra">gazette</punka-media>, or something like that, wink, wink), you'd better go with time to spare because if you don't, you'll end up losing track of time and with no <punka-media data-id="la-gaceta-de-puentechatarra">gazette</punka-media> or anything else. If you had to edit a <punka-media data-id="la-gaceta-de-puentechatarra">Gazette</punka-media>. Not me, for example. Ahem.</p>`,
+                "es": '<p>Trones, vaya viaje más malo que me pegué el mes pasado. Hay un garito en <punka-place>Sobatetas</punka-place> que se llama <punka-place>Cinco Lobitas</punka-place>, que acaba de abrir, y como buen informador que os quiere dar las noticias muy más mejores, fui a investigar por allí. Cinco lobitas no había, pero ya os digo yo que aullidos se oían de puta madre. Buen ambiente, buena peña y una priba... Joer la priba. Un licor casero que entraba solo y en cuanto te levantabas, te ibas al suelo otra vez. Vamos, que muy recomendable el sitio. Habéis de ir. Eso sí, si tenéis algo que hacer (como editar una <punka-media data-id="la-gaceta-de-puentechatarra">Gaceta</punka-media>, o algo así, guiño, guiño), mejor vais con tiempo libre porque si no os pilla el toro y al final ni <punka-media data-id="la-gaceta-de-puentechatarra">Gaceta</punka-media> ni hostias. Si tuvieseis que editar una <punka-media data-id="la-gaceta-de-puentechatarra">Gaceta</punka-media>. Yo no, por ejemplo. Ejem.</p>'
+            },
+            {
+                "en": `<p>I've had to get through a lot of old <punka-media data-id="la-gaceta-de-puentechatarra">Gazettes</punka-media> to find where he'd got out of that <punka-character data-id="la-yoly">Yoly</punka-character>'s crazy idea of <punka-reference data-id="punkapocalyptic-el-juego-de-rol">a book to go on adventures with your mates in your mind</punka-reference>, pretending you're cool when you're actually a big fat prick in his old man's basament.</p><p>And on top of that, it has very few drawings! You have to read almost everything, with the bunch of illiterates we have in this fucking city! And have friends to play with it! FRIENDS! I'm fucking laughing my ass off! Anyway, more than a year ago, she ate a big fat shit this big. Well, now he's managed to sneak it in.</p><p>Yes, guys, she's publishing the fucking book. A rock-playing game, I think they call it. You'll see it around one of these days. At least you can see some explicit illustrations. That's something.</p>`,
+                "es": '<p>Me he tenido que tragar un montón de <punka-media data-id="la-gaceta-de-puentechatarra">Gacetas</punka-media> de las viejas para encontrar dónde se había largado de aquella ida de olla <punka-character>la Yoly</punka-character> de <punka-reference data-id="punkapocalyptic-el-juego-de-rol">un libro para irse de aventuras con tus colegas en tu mente</punka-reference>, de mentira, haciéndote el guay cuando en realidad eres un pringao granudo en el sótano de sus viejos.</p><p>¡Y encima tiene pocos dibujetes! ¡Que lo hay que leer casi todo, con la panda de analfaburros que tenemos en esta puta ciudad! ¡Y tener amigos con los que jugar! ¡JA! ¡Colegas! ¡Es que me parto el puto ojete! En fin, que hace más de un año se comió un ñordo así de alto. Pues ahora lo han conseguido colar.</p><p>Sí, chavales, que lo publica. Un juego de rock, creo que lo llaman. Lo veréis por ahí. por lo menos en los dibujos se ve cacho. Algo es algo.</p>'
+            },
+            {
+                "en": `<p>Some dudes are leaving <punka-place data-id="puentechatarra">Scrapbridge</punka-place> in a few days who are going to cort... carte... cratro... draw the maps of the whole northern area. Apparently these guys are going to find out once and for all if the stories about <punka-nature data-id="mono">monkeys</punka-nature> riding <punka-nature data-id="dinosaurio">dinosaurs</punka-nature> are true or what the fuck is going on up there. The brainiacs going there to study the <punka-place data-id="bosque-vivo">Living Forest</punka-place> and the plants and critters inside it need protection, because they already have enough knowing how to read and add up,so it's in the hands of others to do the bashing. If you need work and bullets, sing up for this mission of uncertain return.</p>`,
+                "es": '<p>Se piran de <punka-place>Puentechatarra</punka-place> en unos dias unos pavos que van a corta... carte... cratro... dibujar los mapas de toda la zona del norte. Por lo visto esta peña va a descubrir de una perra vez si son ciertas las historias de los <punka-nature data-id="mono">monos</punka-nature> en <punka-nature data-id="dinosaurio">dinosaurios</punka-nature> o qué coño pasa. Los cerebrines que van allí a estudiar el <punka-place>Bosque Vivo</punka-place> y las plantas y bichos que hay dentro necesitan protección, porque ellos con saber leer y sumar ya tienen bastante, asi que las hostias las tienen que dar otros. Si necesitáis curro y balas, apuntaos a esta misión con retorno incierto.</p>'
+            }
+        ],
+        "classyfrieds": [
+            {
+                "message": {
+                    "en": '<p>Portraits of faces painted with only two numbers. I also charge two digits.</p>',
+                    "es": '<p>Se pintan retratos de caretos con dos numeros na más. Cobro también dos números de balas.</p>'
+                },
+                "reference": {
+                    "en": 'REF: Draw64',
+                    "es": 'REF: Dibu64'
+                }
+            },
+            {
+                "message": {
+                    "en": '<p>I buy ground chalk or sacks of used plaster. I pay little, you bastards.</p>',
+                    "es": '<p>Compro tiza molida o sacos de yeso usado. Pago poco, cabrones.</p>'
+                },
+                "reference": {
+                    "en": 'REF: <punka-character data-id="empresaurio13">Entrepeneur13</punka-character>',
+                    "es": 'REF: <punka-character>Empresaurio13</punka-character>'
+                }
+            },
+            {
+                "message": {
+                    "en": `<p>Let's see when you stop editing <punka-media data-id="la-gaceta-de-puentechatarra">this fucking shit</punka-media>. You chicks are so unfunny.</p>`,
+                    "es": '<p>A ver cuándo dejáis de editar <punka-media data-id="la-gaceta-de-puentechatarra">esta puta mierda. Poca gracia tenéis, pollastres.</p>'
+                },
+                "reference": {
+                    "en": 'REF: <punka-character data-id="la-yoly">Yoly</punka-character>',
+                    "es": 'REF: <punka-character data-id="la-yoly">Yoly</punka-character>'
+                }
+            },
+            {
+                "message": {
+                    "en": `<p>I'm selling fucking great sniff, the real shit. Proven quality.</p>`,
+                    "es": '<p>Vendo farlopa de puta madre, mierda de la güena. Calidad contrastada.</p>'
+                },
+                "reference": {
+                    "en": 'REF: <punka-character data-id="empresaurio13">Entrepeneur13</punka-character>',
+                    "es": 'REF: <punka-character>Empresaurio13</punka-character>'
+                }
+            }
+        ],
+        "monthly news": {
+            "en": `<p>A lot of wounded and stiffs are arriving these days to <punka-place data-id="puentechatarra">Scrapbridge</punka-place>, because it seems that there was a massive gang fight a few days ago in an area of <punka-place data-id="el-paramo">the Wasteland</punka-place> called <punka-place data-id="ravpicha-o-algo-asin"><punka-reference data-id="ravnica-city-of-gobbo">Ravpicha</punka-reference> or something like that</punka-place>. <punka-faction data-id="v-reich">Reich militants</punka-faction>, <punka-faction data-id="mutardos">mutards</punka-faction>, <punka-faction data-id="hijos-de-la-sangre-negra">blackbloods</punka-faction>, <punka-faction data-id="pandilleros">gangers</punka-faction> and even some of those <punka-faction data-id="idos">Amok</punka-faction> fucks who can't tell friend from foe apart, got into a big mess over several miles of Wasteland doing a lot of things. They were hunting <punka-nature data-id="ojaldron" data-name="Eyedrone">eyedrones</punka-nature>, stealing ungodly books from cemeteries that made one hell of a creep, and even climbed up into the peaks to get some dweebs who live in a hut.</p><p>Well, the Family of the Hut finally smashed them all in the face. <punka-character data-id="padre-canival" data-name="Canival Father">Father</punka-character> and <punka-character data-id="madre-canival" data-name="Canival Mother">Mother</punka-character> went on a rampage, cutting necks, cracking nuts and tearing off limbs with such glee that I doubt if the gangs will want to go back there for a long time. By all accounts of those who came outta there alive, the <punka-faction data-id="idos">Amok</punka-faction> were the best off, because as they didn't care a bit about the mutilations. But come on, it's not to call them "winners" either. The rest of them, being allowed to run away holding back their guts and their tears, that would be enough.</p><p>Hear me out, if you go up there, don't even think about fucking messing with those in the cabin. If you don't exactly know where it is, don't go around busting balls of people who live in cabins. If they live there in the middle of the <punka-place data-id="el-paramo">Wasteland</punka-place> all by themselves, there must be a good reason.</p>`,
+            "es": '<p>Están llegando mogollón de heridos y fiambres estos dias a <punka-place>Puentechatarra</punka-place>, porque parece ser que hubo tremendo fostial de bandas hace unos dias en una zona del <punka-place data-id="el-paramo">Páramo</punka-place> que se llama <punka-place><punka-reference data-id="ravnica-city-of-gobbo">Ravpicha</punka-reference> o algo asín</punka-place>. <punka-faction data-id="v-reich">Militantes del Reich</punka-faction>, <punka-faction>mutardos</punka-faction>, <punka-faction data-id="hijos-de-la-sangre-negra">sangrenegras</punka-faction>, <punka-faction>pandilleros</punka-faction> e incluso taraos de esos de los <punka-faction>Idos</punka-faction> que no distinguen a amigos de enemigos, se piñaron de lo lindo a lo largo y ancho de varios kilómetros de páramo haciendo un montón de cosas. Cazaron <punka-nature data-id="ojaldron" data-name="Ojaldrón">ojaldrones</punka-nature>, robaron libros impíos de cementerios que daban un canguelo de la hostia, e incluso subieron a los picos a por unos pringaos que viven en una cabaña.</p><p>Pues la familia de la Cabaña al final les partió la cara a todos. Se pusieron <punka-character data-id="padre-canival" data-name="Padre Caníval">Padre</punka-character> y <punka-character data-id="madre-canival" data-name="Madre Caníval">Madre</punka-character> a repartir hostias como panes, cortaron cuellos, chafaron huevos y arrancaron miembros con una alegría que dudo mucho que a las bandas les apetezca volver por allí en una buena temporada. Según cuentan, de entre los que salieron de allí los <punka-faction>Idos</punka-faction> fueron los mejor parados, porque como ya están mal de la puta olla de por sí, pues como que les daban un poco igual las mutilaciones, pero vamos, que tampoco es pa llamarlos "ganadores". Los demás, con salir por patas sujetándose las tripas y aguantándose los lloros, pues suficiente.</p><p>Vamos, que si subís por allí, ni de puta coña se os ocurra ir a tocarles los cojones a los de la Cabaña. Si no sabéis dónde está exactamente, pues no vayáis por ahí tocando los cojones alegremente a peña que vive en cabañas. Que si en pleno <punka-place data-id="el-paramo">Páramo</punka-place> viven ellos ahí solos, por algo será.</p>'
+        }
+    },
+    "type": "gazette"
+});
