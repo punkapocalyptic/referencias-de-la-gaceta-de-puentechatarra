96068a7c2a23c1b8a8024379b6bd9470e94be272
punkapocalyptic <67438405+punkapocalyptic@users.noreply.github.com>
2022-08-02 23:06:25 +0200

    Gaceta 75
    Introducción de la Gaceta 75 y mejora de la 73.

---
+++ js/source/gazettes/75.js
@@ -0,0 +1,56 @@
+source.push({
+    "id": 75,
+    "sections": {
+        "breaking news": [
+            {
+                "en": `<p>Ok guys, it’s been two months since we’ve published this <punka-media data-id="la-gaceta-de-puentechatarra">Gazzette</punka-media> that makes your life happier... or unhappier... whatever. The fact is that we organized <punka-reference data-id="punkachopo" data-name="Punkachopo">a meeting to meet</punka-reference> with some of our fans, chat a bit, do some activities and while we’re at it, eat some <punka-foodstuff>punkachopo</punka-foodstuff>. What’s a <punka-foodstuff>punkachopo</punka-foodstuff>? Let me explain it to you. It’s like if you take a <punka-nature data-id="bufamello">buffalo</punka-nature>, bread it and fry it, with cheese and cured ham inside it like a meat envelope. And very well done on top of it.</p><p>But of course, in the end we didn’t do anything of the planned activities but eating, and we completed a really pleasant weekend. And I’ve been digesting all this time. Buff, I haven’t had a worse time in my life... but I’m looking forward to next year’s <punka-foodstuff>punkachopo</punka-foodstuff>.</p>`,
+                "es": '<p>Vale tíos, llevamos dos meses sin sacar <punka-media data-id="la-gaceta-de-puentechatarra">esta publicación</punka-media> que os hace la vida más feliz... o desgraciada... o lo que sea. El caso es que <punka-reference data-id="punkachopo" data-name="Punkachopo">organizamos una reunión para reunirnos</punka-reference>, que para eso son las reuniones, con algunos de nuestros fanses, charlar un poco, realizar actividades y de paso zamparnos algún <punka-foodstuff>punkachopo</punka-foodstuff>. ¿Que qué es un <punka-foodstuff>punkachopo</punka-foodstuff>? A ver cómo os lo explico. Es cómo si pillaras un <punka-nature>bufamello</punka-nature>, lo empanaras y lo frieras, con alguna cosa ahí dentro. Pero encima bien hecho.</p><p>Y claro, al final pasamos de todo menos de zampar y nos pusimos como auténticos cerdacos. Y llevo todo este tiempo de digestión. Buff, no lo he pasado peor en mi vida... pero estoy deseando que enganchar el <punka-foodstuff>punkachopo</punka-foodstuff> del año que viene.</p>'
+            },
+            {
+                "en": `<p>There are rumors coming in from many wastelanders of a place called the <punka-place data-id="estrecho-humedo">Wet Strait</punka-place>. The thing is that everyone claims to know it and that this place exists, but then no one remembers exactly where it is, or if they saw it or a friend told them about it.</p><p>And now that I mention it, it does ring a bell that there is a place called like that. Or if not there must be because that name deserves it.</p>`,
+                "es": '<p>Nos están llegando rumores de muchos parameños de un lugar llamado el <punka-place>Estrecho Húmedo</punka-place>. El caso es que todos dicen conocerlo y que ese sitio existe, pero luego nadie se acuerda dónde está exactamente, o si lo vieron ellos o se lo contó un amigo.</p><p>Y es que ahora que lo pongo sí que me suena que hay un sitio que se llama así. O si no tiene que haberlo porque ese nombre lo merece.</p>'
+            },
+            {
+                "en": `<p>They inform us of a real milestone in the history of <punka-place data-id="puentechatarra">Scrapbridge</punka-place>, and more specifically in the <punka-place data-id="sobaco">Armpit neighborhood</punka-place>. It turns out that in <punka-place>Whiskeydicks's</punka-place>, that tavern that attracts the worst of the worst of this settlement, have been three full days without any fucking fight. And I'm not even talking about the massive brawls that sometimes take place, where there are more people getting into each other's faces than in a <punka-event data-id="batalla del Páramo">Wasteland Battle Royal</punka-event>. No, no, not even a miserable punch, not even a pushin' against the furniture... and almost not even a miserable bad look.</p><p>People in the neighborhood are extremely upset because something like this cannot be a coincidence. What if they are putting something in the drink? What if they are not putting it now, but they used to? Okay, now you go there with a lot more tranquility, but after a while you realize that without the drinks the place is a fucking pain in the ass... or maybe your buddies are, but come on, you’ll get a really bad feeling.</p>`,
+                "es": `<p>Nos informan de un verdadero hito en la historia de <punka-place>Puentechatarra</punka-place>, y más concretamente en el <punka-place data-id="sobaco">barrio del Sobaco</punka-place>. Resulta que en <punka-place>Whiskeydicks's</punka-place>, esa taberna que atrae a lo peor de lo peor de este asentamiento, llevan tres días completos sin ninguna puta pelea. Y no hablo ya de los movidones multitudinarios que se suelen montar a veces que hay más gente fostiándose que en una <punka-event>batalla del Páramo</punka-event>. No, no, es que ni un miserable puñetazo a desgana, ni un empujón contra el mobiliario... y casi ni una mala mirada.</p><p>La gente del barrio está sumamente mosqueada porque algo así no puede ser casualidad. ¿Y si le están poniendo algo a la bebida? ¿Y si ahora no lo están poniendo pero lo ponían antes? Que vale, ahora uno va allí con mucha más tranquilidad, pero al rato te das cuenta que sin las hostias el sitio es un puto coñazo... o igual lo son tus colegas, pero vamos, que te entra un bajonazo de flipar.</p>`
+            }
+        ],
+        "classyfrieds": [
+            {
+                "message": {
+                    "en": '<p>Getting your hands on an <punka-nature data-id="lameojetes">aholelicker</punka-nature> is too expensive for ya? I do the same job, but cheaper.</p>',
+                    "es": '<p>¿Conseguir un <punka-nature>lameojetes</punka-nature> es demasiado caro para ti? Yo hago el mismo trabajo y más barato.</p>'
+                },
+                "reference": {
+                    "en": `REF: <punka-character data-id="llambion">Lickin'</punka-character>`,
+                    "es": 'REF: <punka-character>Llambión</punka-character>'
+                }
+            },
+            {
+                "message": {
+                    "en": `<p>I can't wait for ya teeth to fall off, I will tear ‘em apart in ya sleep.</p>`,
+                    "es": '<p>No pienso esperar a que sus caigan, os voy a arrancar los dientes mientras dormís.</p>'
+                },
+                "reference": {
+                    "en": 'REF: <punka-character data-id="aguaron-perez">Tooth Beast</punka-character>',
+                    "es": 'REF: <punka-character>Aguarón Pérez</punka-character>'
+                }
+            },
+            {
+                "message": {
+                    "en": `<p>For sale, slices of <punka-troop data-id="cabezon">bobblehead</punka-troop> brains. To make clever experiments or eat as a delicatesen. Hurry up, selling fast.</p>`,
+                    "es": '<p>Vendo rodajas de cerebro de <punka-troop>Cabezón</punka-troop>. Para experimentación o delicatesen. Darse prisa que se acaban.</p>'
+                },
+                "reference": {
+                    "en": 'REF: <punka-character>Chef Doc</punka-character>',
+                    "es": 'REF: <punka-character>Chef Doc</punka-character>'
+                }
+            }
+        ],
+        "monthly news": {
+            "en": `<p>Do you know what usually happens when things seem to be going really horrible and then they get worse? Well, for fucking once the opposite has happened and we have found the best thing that could possibly happen.</p><p>The first groups of <punka-faction data-id="idos">Amok</punka-faction> and <punka-faction data-id="irradiados">Irradiated</punka-faction> are meeting and they are beating each other's crap outta their sorry asses. Between some whose only goal is to torture and kill, and others who are more into irradiating and killing, the areas where they meet end up looking like the <punka-place data-id="baldio-final">Final Wasteland</punka-place> in its good ole days. So everyone's hope is that they annihilate each other for fuck's sake. Let's see, realistically and given the luck we have as a rule, in the end they even become best buddies and get together to leave the whole <punka-place>Wasteland</punka-place> devastated. But well, let's leave them alone for now and let them raze each other to the ground.</p><p>It seems that for now these battles are only happening south of the <punka-place data-id="gran-raja">Great Rift</punka-place>. And although fortunately there are not many settlements nearby, it is affecting trade significantly. On one hand, in <punka-place data-id="vientocho">Fatwind</punka-place> they have decided to destroy part of the passage made of boats and footbridges that led from the mainland to their island, and remain isolated from all this bloody mess. On the other hand, the usual routes to the area of <punka-place>Merkadome</punka-place> have become very dangerous and are taking monstrous detours even farther away from <punka-place data-id="torresbrillantes">Gleaming Towers</punka-place> and now passing near <punka-place>Samanthia</punka-place>, all to avoid encountering these weird fuckers. Although these territories used to be avoided because they were considered less safe, now they are starting to be a lesser evil. Shite.</p>`,
+            "es": '<p>¿Sabéis lo que suele pasar normalmente de que las cosas parecen ir realmente horribles y que en ese momento lo que hacen es empeorar? Pues hostia puta, que por una vez ha ocurrido al contrario y nos hemos encontrado lo mejor que podía pasar.</p><p>Se están encontrando los primeros grupos de <punka-faction>Idos</punka-faction> y de <punka-faction>Irradiados</punka-faction> y se están dando de hostias hasta en el paladar. Entre unos que su única meta es torturar y matar, y otros que son más de irradiar y matar, las zonas donde se encuentran acaban pareciendo el <punka-place>Baldío Final</punka-place> en sus buenos tiempos. Así que la esperanza de todos es que se aniquilen entre ellos de una puta vez. Que vamos a ver, siendo realistas y vista la suerte que tenemos de normal, al final hasta se hacen los mejores amiguetes y se arrejuntan para dejar todo el <punka-place>Páramo</punka-place> arrasado. Pero bueno, por ahora dejémosles tranquilos arrasándose entre ellos.</p><p>Parece que por ahora estos encuentros se concentran al sur de la <punka-place>Gran Raja</punka-place>. Y aunque afortunadamente no hay muchos asentamientos cercanos, sí que está afectando de manera importante al comercio. Por una parte en <punka-place>Vientocho</punka-place> han decidido destrozar parte del paso hecho a base de barcos y pasarelas que llevaba desde tierra firme hasta su isla, y quedarse aislados de toda esta quincalla. Por otra, las rutas habituales a la zona de <punka-place>Merkadome</punka-place> se han vuelto muy peligrosas y se están desviando una bestialidad alejándose incluso de <punka-place>Torresbrillantes</punka-place> y pasando ahora cerca de <punka-place>Samanthia</punka-place>, todo para no encontrarse con esta gente. Que aunque esos territorios antes se evitaban por menos seguros, ahora empiezan a ser un mal menor.</p>'
+        }
+    },
+    "type": "gazette"
+});
